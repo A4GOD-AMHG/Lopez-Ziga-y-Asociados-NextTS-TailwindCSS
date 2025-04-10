2681ede74791d1739a04f2163d6f37135600f425
a4god- <alexismhgarcia@gmail.com>
2025-04-10 05:29:01 -0400

    changed api methods

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"cos4aEmhvF3tgha7jrZQ9TzO4qef/KMAXbHenWcPT7s=\"\n}"
--- src/app/api/newsletter/route.ts
+++ src/app/api/newsletter/route.ts
@@ -1,17 +1,17 @@
 import { NextResponse } from 'next/server';
-import { Resend } from 'resend';
+// import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(request: Request) {
     const { email } = await request.json();
 
     try {
-        await resend.contacts.create({
-            email,
-            audienceId: process.env.RESEND_AUDIENCE_ID!,
-        });
-
+        // await resend.contacts.create({
+        //     email,
+        //     audienceId: process.env.RESEND_AUDIENCE_ID!,
+        // });
+        console.log(email)
         return NextResponse.json({ success: true });
     } catch (error) {
         return NextResponse.json(
