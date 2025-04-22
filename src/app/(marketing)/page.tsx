b56cfb2b7db38e692657b4d00b47e4a7293340da
a4god- <alexismhgarcia@gmail.com>
2025-04-22 01:47:56 -0400

    changed image

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -12,9 +12,7 @@ export default async function HomePage() {
                     src="/images/law-bg.avif"
                     priority
                     blurDataURL='/images/law-bg.avif'
-                    width={1350}
-                    height={658}
-                    sizes="100vw"
+                    fill
                 />
                 <div className="parallax-overlay" />
                 <div className="container mx-auto px-4 relative z-10">
